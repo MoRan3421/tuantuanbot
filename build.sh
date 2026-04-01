56efe2180ede809db1dc97c689f46d406df48489
团团熊猫 <111855367+MoRan3421@users.noreply.github.com>
2026-04-02 01:44:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,13 +10,5 @@ pip install --upgrade pip
 # 2. 安装依赖库
 pip install -r requirements.txt
 
-# 3. 检查 cogs 目录
-if [ ! -d "cogs" ]; then
-  echo "正在创建 cogs 目录..."
-  mkdir cogs
-fi
-
-echo "--- 构建完成喵！ ---"
-
 #启动机器人
 python main.py
